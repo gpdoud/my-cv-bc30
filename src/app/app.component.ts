245369b125d85c9afb524ef691410701c279c103
Greg Doud <gpdoud@gmail.com>
2020-11-09 16:56:42 -0500

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-cv';
+  name: string = "Greg Doud";
+  address: string = "4900 Parkway Dr.";
+  cityStZip: string = "Mason, OH 45040 ";
+  phone: string = "513-322-8888 ";
+  email: string = "gdoud@maxtrain.com";
+  workExperience: string = "This is the work experience";
+  education: string[] = [
+    "Mt St. Mary of the West",
+    "UC"
+  ];
 }
